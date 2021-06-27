037244fc415c9757093193abd75c5718d892c39b
akshayvinodn <84909991+akshayvinodn@users.noreply.github.com>
2021-06-27 14:51:08 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles.css'
+
+function Header() {
+    return (
+        <div>
+            <header className='App-header' style={{color:'blue'}}>
+                <h1>Shape AI Bootcamp</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Header
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles.css'
+function Info() {
+    return (
+    <div className="note">
+        <h1 style={{color:'white'}}>Javascript and React.js</h1>
+        <p style={{color:'red'}}>A basic web dev React Js BootCamp</p>
+    </div>
+    )
+}
+
+export default Info
+
